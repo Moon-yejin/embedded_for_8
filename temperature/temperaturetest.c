d292137b38ac07f8f513bc94eb1a7feb9cee2422
ParkHyunseo2000 <t7yg4hb5n@naver.com>
2022-12-02 11:29:55 +0900

    오타 수정
    temperature.c에 있는 여러 오타를 수정하였다.
    temperature.c의 main함수를 temperaturetest.c로 옮겼다.

--- temperature/temperaturetest.c
+++ temperature/temperaturetest.c
@@ -11,4 +11,26 @@
 #include <linux/spi/spidev.h>
 #include "temperature.h"
 
+int main(int argc, char **argv)
+{
+        char *buffer;
+        int file;
+        file = spi_init("/dev/spidev1.0");
+        buffer = (char*)spi_read_lm74(file);
+        close(file);
+
+        int value = 0;
+        value = (buffer[1] >> 3);
+        value += ((int)(buffer[0])) << 5;
+        if(buffer[0] & 0x80)
+        {
+                int i=0;
+                for(i=31; i>12; i--)
+                value |= (1<<i);
+        }
+
+        double temp = (double)value*0.0625;
+        printf("Current Temp: %lf \n", temp);
+        return 0;
+}
 
