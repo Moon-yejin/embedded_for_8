30f08e58d08e05ee5c29ca9f427ad8067cd13644
ParkHyunseo2000 <t7yg4hb5n@naver.com>
2022-12-02 12:16:56 +0900

    temperature.c에 새로운 함수 추가
    temperature.c에 read_temperature()함수를 추가하였다.

--- temperature/temperature.c
+++ temperature/temperature.c
@@ -9,6 +9,7 @@
 #include <sys/ioctl.h>
 #include <linux/types.h>
 #include <linux/spi/spidev.h>
+#include "temperature.h"
 
 char gbuf[10];
 int spi_init(char filename[40])
@@ -37,3 +38,25 @@ char *spi_read_lm74(int file)
 	return gbuf;
 }
 
+void read_teamperature(void)
+{
+	char *buffer;
+	int file;
+	file = spi_init("/dev/spidev1.0");
+	buffer = (char*)spi_read_lm74(file);
+	close(file);
+
+	int value = 0;
+	value = (buffer[1] >> 3);
+	value += ((int)(buffer[0])) << 5;
+	if(buffer[0] & 0x80)
+	{
+		int i=0;
+		for(i=31; i>12; i--)
+		value |= (1<<i);
+	}
+
+	double temp = (double)value*0.0625;
+	printf("Current Temp: %lf \n", temp);
+	return 0;
+}
--- temperature/temperature.h
+++ temperature/temperature.h
@@ -1,10 +1,9 @@
 #ifndef _TEMPERATURE_H_
 #define _TEMPERATURE_H_
 
-char gbuf{10};
 int spi_init(char filename[40]);
-int file;
 char *spi_read_lm74(int file);
+void read_temperature(void);
 
 
 #endif
